ffe2b9769c760c736cec2a31c228f02c8fbd122e
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-02-25 19:27:59 -0300

    chore: mudanças nos componentes da home page

--- frontend/leilao-app/src/app/app.module.ts
+++ frontend/leilao-app/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './homePage/header/header.component';
+import { HomeComponent } from './homePage/home/home.component';
+import { FooterComponent } from './homePage/footer/footer.component';
+import { SliderComponent } from './homePage/slider/slider.component';
+import { MostViewedComponent } from './homePage/most-viewed/most-viewed.component';
+import { AuctionsHomePageComponent } from './homePage/auctions-home-page/auctions-home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SliderComponent,
+    MostViewedComponent,
+    AuctionsHomePageComponent
   ],
   imports: [
     BrowserModule,
