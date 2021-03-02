1e1cab8d172530a7750699745065f77a2ca3824f
Gabriel Bellagamba <gabriel.chaus@gmail.com>
2021-03-02 18:27:42 -0300

    prototipo conexao com banco em multi servidor, principio de multi servico

--- frontend/leilao-app/src/app/app.module.ts
+++ frontend/leilao-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
