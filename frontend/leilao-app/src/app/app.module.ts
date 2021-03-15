3930564370ab3a6431006f099660c6794e40cee8
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-03-15 01:18:22 -0300

    fix: correção de conflitos

--- frontend/leilao-app/src/app/app.module.ts
+++ frontend/leilao-app/src/app/app.module.ts
@@ -1,39 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< Updated upstream
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
+
+
+
 import { LoginComponent } from './login/login.component';
 import { CardComponent } from './card/card.component';
-=======
-import { HeaderComponent } from './homePage/header/header.component';
-=======
+
+
+
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router'
 
 import { ROUTES } from './app.routes'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
->>>>>>> Stashed changes
+
 import { HomeComponent } from './homePage/home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { SliderComponent } from './homePage/slider/slider.component';
 import { MostViewedComponent } from './homePage/most-viewed/most-viewed.component';
 import { AuctionsHomePageComponent } from './homePage/auctions-home-page/auctions-home-page.component';
-<<<<<<< Updated upstream
->>>>>>> 1996a38c2c7fafcc69c0e6947441fe0d1c0b0569
-=======
-import { AuctionCardMostViwedComponent } from './homePage/auction-card-most-viwed/auction-card-most-viwed.component';
+
+//import { AuctionCardMostViwedComponent } from './homePage/auction-card-most-viwed/auction-card-most-viwed.component';
 import { SearchAuctionsComponent } from './auctions/searchAuctions/search-auctions.component';
 
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
@@ -41,20 +36,14 @@ import { SearchAuctionsComponent } from './auctions/searchAuctions/search-auctio
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-<<<<<<< HEAD
     LoginComponent,
-    CardComponent
-=======
+    CardComponent,
     SliderComponent,
     MostViewedComponent,
-<<<<<<< Updated upstream
-    AuctionsHomePageComponent
->>>>>>> 1996a38c2c7fafcc69c0e6947441fe0d1c0b0569
-=======
     AuctionsHomePageComponent,
-    AuctionCardMostViwedComponent,
+    AuctionsHomePageComponent,
+   // AuctionCardMostViwedComponent,
     SearchAuctionsComponent,
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
