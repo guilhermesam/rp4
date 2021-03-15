8c2b2cb7a057f27f2584d7dcd0d0055c4d9702c1
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-03-15 01:18:22 -0300

    fix: correção de conflitos

---
+++ frontend/leilao-app/src/app/auction.services.ts
@@ -0,0 +1,15 @@
+import { HttpClient, JsonpClientBackend } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Items } from '../shared/items.model'
+
+@Injectable()
+export class AuctionsService {
+    constructor(private http : HttpClient) { }
+    public items : Items[]
+    public getAuctions(): Promise<Items[]>{
+        return this.http.get('http://localhost:3333/items/search/all')
+            .toPromise()
+            .then((resposta: Items[]) => resposta)
+            
+    }
+}
--- frontend/leilao-app/src/app/homePage/auctions-home-page/auctions-home-page.component.ts
+++ frontend/leilao-app/src/app/homePage/auctions-home-page/auctions-home-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuctionsService } from 'src/app/auction.services';
+import { Items } from 'src/shared/items.model';
 
 @Component({
   selector: 'app-auctions-home-page',
   templateUrl: './auctions-home-page.component.html',
-  styleUrls: ['./auctions-home-page.component.css']
+  styleUrls: ['./auctions-home-page.component.css'],
+  providers: [ AuctionsService ]
 })
 export class AuctionsHomePageComponent implements OnInit {
 
-  constructor() { }
+  public items : Items[]
+
+  constructor(private aucticonsServices: AuctionsService) { }
 
   ngOnInit(): void {
+    this.aucticonsServices.getAuctions()
+      .then((items : Items[])=>{
+        this.items = items
+      })
+      .catch((param : any) =>{
+        console.log(param)
+      })
+
+      
   }
 
 }
