f551f7b2f32a24c53cad32666c55d8ff6f0f4d6d
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-03-21 17:48:06 -0300

    feat: acesso a pagina de um leilão

--- frontend/leilao-app/src/app/app.routes.ts
+++ frontend/leilao-app/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router'
 
 import { HomeComponent } from './homePage/home/home.component';
 import { SearchAuctionsComponent } from './auctions/searchAuctions/search-auctions.component';
+import { AuctionComponent } from './auctions/auction/auction/auction.component'
+import { LoginComponent } from './login/login.component'
 
 export const ROUTES:  Routes = [
     {path : '', component: HomeComponent},
-    {path : 'buscar-leiloes', component: SearchAuctionsComponent}
-]
+    {path : 'buscar-leiloes', component: SearchAuctionsComponent},
+    {path : 'leilao', component: AuctionComponent},
+    {path : 'leilao/:id', component: AuctionComponent},
+    {path : 'login', component: LoginComponent}
+] 
