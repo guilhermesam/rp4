02c62a48e0a5212c0bf4819e6f608f2cc7fd4d60
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-03-21 10:18:17 -0300

    feat: adição das categorias na pagina de busca

---
+++ backend/src/useCases/ManageCategories/SearchCategory/SearchCategoryController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import { IStrategy } from './IStrategy'
+import SearchCategoryUseCase from './SearchCategoryUseCase'
+
+class SearchCategoryController {
+  private strategy: IStrategy
+
+  setStrategy (strategy: IStrategy) {
+    this.strategy = strategy
+  }
+
+  async handle (request: Request, response: Response) {
+    try {
+      const items = await this.strategy.search(new SearchCategoryUseCase())
+
+      return response.status(200).json(items)
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message || 'Unexpected error!'
+      })
+    }
+  }
+}
+
+export default new SearchCategoryController()
--- frontend/leilao-app/src/app/auction.services.ts
+++ frontend/leilao-app/src/app/auction.services.ts
@@ -1,15 +1,26 @@
 import { HttpClient, JsonpClientBackend } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Category } from 'src/shared/categories.model'
 import { Items } from '../shared/items.model'
 
 @Injectable()
 export class AuctionsService {
     constructor(private http : HttpClient) { }
+    
     public items : Items[]
+    public category: Category[]
+    
     public getAuctions(): Promise<Items[]>{
         return this.http.get('http://localhost:3333/items/search/all')
             .toPromise()
             .then((resposta: Items[]) => resposta)
+            
+    }
+
+    public getCategories(): Promise<Category[]>{
+        return this.http.get('http://localhost:3333/items/search/allCategories')
+            .toPromise()
+            .then((resposta: Category[]) => resposta)
             
     }
 }
