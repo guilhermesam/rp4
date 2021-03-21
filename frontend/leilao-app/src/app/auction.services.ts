84c96788386e5404f1c3f7906301958a1fc542b8
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-03-21 17:48:06 -0300

    feat: acesso a pagina de um leilão

--- frontend/leilao-app/src/app/auction.services.ts
+++ frontend/leilao-app/src/app/auction.services.ts
@@ -13,14 +13,22 @@ export class AuctionsService {
     public getAuctions(): Promise<Items[]>{
         return this.http.get('http://localhost:3333/items/search/all')
             .toPromise()
-            .then((resposta: Items[]) => resposta)
+            .then((response: Items[]) => response)
             
     }
 
     public getCategories(): Promise<Category[]>{
         return this.http.get('http://localhost:3333/items/search/allCategories')
             .toPromise()
-            .then((resposta: Category[]) => resposta)
+            .then((response: Category[]) => response)
             
     }
+
+    public getAuctionsByID(id: string): Promise<Items>{
+        return this.http.get('http://localhost:3333/items/search/all?id=${id}')
+        .toPromise()
+        .then((response: Items) => {
+            return response[0]
+        })
+    }  
 }
