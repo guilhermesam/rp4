405b09db6edbc4bb9ed8edf6428a46e8804c9d2a
Bruno Afonso <brunohafonso@gmail.com>
2021-04-11 12:07:40 -0300

    refactor: adjust the database connection

--- backend/src/repositories/connect.ts
+++ backend/src/repositories/connect.ts
@@ -1,5 +1,5 @@
-import { createConnection } from 'typeorm'
+import { Connection, createConnection } from 'typeorm'
 
-createConnection().then(() => {
-  console.log('Successfully connected to database!')
-})
+export default async (): Promise<Connection> => {
+  return createConnection()
+}
